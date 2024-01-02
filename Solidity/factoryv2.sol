60e99a4431760423004b92d5cd579eca9c0724e8
ClayYu <32875832+yuhuajing@users.noreply.github.com>
2024-01-02 15:39:22 +0800

    Update factoryv2.sol

--- Solidity/factoryv2.sol
+++ Solidity/factoryv2.sol
@@ -12,28 +12,59 @@ contract Factory {
         bytes salt
     );
 
-    address[] userwallet;
-
-    function getWalletByIndex(uint256 index) public view returns (address) {
-        require(index < userwallet.length, "Invalid index");
-        return userwallet[index];
-    }
+    address[] public userwallet;
+    address public implementation;
+    address public manager;
+    address public owner;
 
     function getWalletLength() public view returns (uint256) {
         return userwallet.length;
     }
 
-    constructor() payable {}
+    constructor(address _manager, address _implement) payable {
+        owner = msg.sender;
+        manager = _manager;
+        implementation = _implement;
+    }
 
-    function getCreationCode(bytes32 _identifier, address _implementation)
+    modifier onlyOwner() {
+        require(msg.sender == owner, "NOT_OWNER");
+        _;
+    }
+
+    function updateOwner(address newOwner) external onlyOwner {
+        require(
+            newOwner != address(0) && newOwner != owner,
+            "DUP/INVALID_NEWOWNER"
+        );
+        owner = newOwner;
+    }
+
+    function updateManager(address newManager) external onlyOwner {
+        require(
+            newManager != address(0) && newManager != manager,
+            "DUP/INVALID_NEWMANAGER"
+        );
+        manager = newManager;
+    }
+
+    function updateImpl(address newImpl) external onlyOwner {
+        require(
+            newImpl != address(0) && newImpl != implementation,
+            "DUP/INVALID_NEWIMPL"
+        );
+        implementation = newImpl;
+    }
+
+    function getCreationCode(bytes32 _identifier)
         internal
-        pure
+        view
         returns (bytes memory)
     {
         return
             abi.encodePacked(
                 hex"3d60ad80600a3d3981f3363d3d373d3d3d363d73",
-                _implementation,
+                implementation,
                 hex"5af43d82803e903d91602b57fd5bf3",
                 abi.encode(_identifier)
             );
@@ -41,32 +72,28 @@ contract Factory {
 
     function createAccount(
         address _owner,
-        address _implementation,
-        address _manager,
         address _signer,
         uint256 _salt,
         string memory _identifier
     ) external returns (address) {
-        if (_manager == address(0)) revert InvalidManagerInput();
         if (_signer == address(0)) revert InvalidSignerInput();
         bytes memory code = getCreationCode(
-            convertStringToByte32(_identifier),
-            _implementation
+            convertStringToByte32(_identifier)
         );
         bytes memory salt = abi.encode(
             _salt,
             _identifier,
             _owner,
             _signer,
-            _manager,
-            _implementation
+            manager,
+            implementation
         );
         address _account = Create2.computeAddress(
             bytes32(salt),
             keccak256(code)
         );
         if (_account.code.length != 0) return _account;
-        emit AccountCreated(_account, _implementation, salt);
+        emit AccountCreated(_account, implementation, salt);
         assembly {
             _account := create2(0, add(code, 0x20), mload(code), salt)
         }
@@ -75,8 +102,8 @@ contract Factory {
             abi.encodeWithSignature(
                 "initData(address,address,address)",
                 _owner,
-                _manager,
-                _signer 
+                manager,
+                _signer
             )
         );
         if (!success) {
@@ -90,23 +117,20 @@ contract Factory {
 
     function account(
         address _owner,
-        address _implementation,
-        address _manager,
         address _signer,
         uint256 _salt,
         string memory _identifier
     ) external view returns (address) {
         bytes memory code = getCreationCode(
-            convertStringToByte32(_identifier),
-            _implementation
+            convertStringToByte32(_identifier)
         );
         bytes memory salt = abi.encode(
             _salt,
             _identifier,
             _owner,
             _signer,
-            _manager,
-            _implementation
+            manager,
+            implementation
         );
         address _account = Create2.computeAddress(
             bytes32(salt),
