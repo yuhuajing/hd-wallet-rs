60c8731e203f74a8e698b3d5072ae46b8e9794f6
ClayYu <32875832+yuhuajing@users.noreply.github.com>
2024-01-02 17:07:51 +0800

    Update NFT721.sol

--- Solidity/NFT721.sol
+++ Solidity/NFT721.sol
@@ -1,1 +1,19 @@
-1
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+
+import "erc721a/contracts/ERC721A.sol";
+
+contract SC721 is ERC721A {
+    address public owner;
+
+    constructor(string memory name, string memory symbol) payable ERC721A(name,symbol) {
+        owner = msg.sender;
+    }
+
+    function mint(
+        address receiver,
+        uint256 amount
+    ) public {
+        _mint(receiver, amount);
+    }
+}
