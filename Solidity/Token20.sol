c36b2ebced39618f3b97c291781fe8ded9f623a2
ClayYu <32875832+yuhuajing@users.noreply.github.com>
2024-01-02 17:13:19 +0800

    Update Token20.sol

--- Solidity/Token20.sol
+++ Solidity/Token20.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SC20 is ERC20 {
+    address public owner;
+
+    constructor(string memory name, string memory symbol) payable ERC20(name,symbol) {
+        owner = msg.sender;
+    }
+
+    function mint(
+        address receiver,
+        uint256 amount
+    ) public {
+        _mint(receiver, amount);
+    }
+}
 
