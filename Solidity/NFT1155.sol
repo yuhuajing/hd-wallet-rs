3ceaa1ff415655dbb9b5c645088fd7c44b21ff1c
ClayYu <32875832+yuhuajing@users.noreply.github.com>
2024-01-02 17:03:52 +0800

    Update NFT1155.sol

--- Solidity/NFT1155.sol
+++ Solidity/NFT1155.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract SC1155 is ERC1155 {
+    address public owner;
+
+    constructor() payable ERC1155("") {
+        owner = msg.sender;
+    }
+
+    function mint(
+        address receiver,
+        uint256 tokenid,
+        uint256 amount
+    ) public {
+        _mint(receiver, tokenid, amount, bytes(""));
+    }
+}
 
